569e9a40157c76e7695be81d0724562e6d8d3987
Yuriy Paskar <85489353+Askarunro@users.noreply.github.com>
2021-12-17 13:09:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './sass/main.scss';
 import './js/script';
-import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
