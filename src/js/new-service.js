61325dc4db6030b5aad49ca111e4b7deb5ac5cce
Yuriy Paskar <85489353+Askarunro@users.noreply.github.com>
2021-12-17 12:08:57 +0200

    update

---
+++ src/js/new-service.js
@@ -0,0 +1,55 @@
+const API_KEY = '24828507-89537ba0cc73f2aa36f96abcf'
+
+import axios from 'axios';
+
+export default class NewsApiServece {
+    constructor() {
+        this.searchName = '';
+        this.page = 1;
+    }
+
+
+    async fetchArticles() {
+        try {
+            const response = await axios({
+                method: 'GET',
+                url: `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchName}&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`,
+            })
+            const todoItems = await response.data;
+            const n = await this.incrementPage();
+            return await todoItems.hits
+        } catch (errors) {
+            console.error(errors);
+        }
+    };
+
+    // FETCH
+
+    // fetchArticles() {
+    //     return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${this.searchName}&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true&per_page`).then(response => {
+    //         if (!response.ok) {
+    //             throw new Error(response.status);
+    //         }
+    //         return response.json();
+    //     }).then(({ hits }) => {
+    //         this.incrementPage();
+    //         if (hits.length < 1) {
+    //             throw new Error(response.status);
+    //         }
+    //         return hits
+    //     });
+    // }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchName;
+    }
+
+    set query(newQuery) {
+        this.searchName = newQuery;
+    }
+}
---
+++ src/js/script.js
@@ -0,0 +1,84 @@
+import debounce from 'lodash.debounce';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+import animalCard from '../templates/card.hbs';
+import Notiflix from 'notiflix';
+import NewsApiService from '../js/new-service';
+
+
+const newsApiService = new NewsApiService();
+const refs = {
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    btnMore: document.querySelector('.btn-more')
+}
+refs.form.addEventListener('submit', onFormSubmit)
+refs.btnMore.addEventListener('click', onBtnClick)
+
+function onBtnClick(evt) {
+    evt.preventDefault();
+    newsApiService.fetchArticles().then(markupHits)
+
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    let a
+    const formData = new FormData(evt.currentTarget)
+    formData.forEach((name) => {
+        a = name;
+        return a
+    })
+    newsApiService.query = a;
+    newsApiService.resetPage();
+    clearArticlesContainer()
+    newsApiService.fetchArticles().then(markupHits).catch(onFetchError)
+
+}
+
+function markupHits(hits) {
+
+    const markup = animalCard(hits);
+    // refs.gallery.innerHTML = markup;
+    refs.gallery.insertAdjacentHTML('beforeend', markup)
+    lightbox.refresh();
+}
+
+function onFetchError(error) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function onFetchOops(oops) {
+    clearCountries();
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    nav: true,
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    infinite: false,
+});
+
+function clearArticlesContainer() {
+    refs.gallery.innerHTML = '';
+}
+
+
+
+// const url = 'https://pixabay.com/api';
+// const options = {
+//     headers: {
+//         key: `${API_KEY}`,
+//         q: 'cat',
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: 'true',
+//          per_page:40,
+//     }
+// }
